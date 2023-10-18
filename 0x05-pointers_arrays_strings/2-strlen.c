e6b6fbae3a35daf54ac8cda21bbbc127950f1da9
BlassedHillary <nnamanihillaryuchenna@gmail.com>
2023-10-17 18:53:46 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Returns cjjd skks
+ * @str: The ste djdd ss
+ * Return: the djdjm
+ */
+
+size_t _strlen(const char *str)
+
+{
+	size_t length = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+}
