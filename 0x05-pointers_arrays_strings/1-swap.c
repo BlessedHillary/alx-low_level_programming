25388d3b262a5ce7174db75b10fd5d4e492b75ca
BlassedHillary <nnamanihillaryuchenna@gmail.com>
2023-10-17 19:28:43 -0700

    1 swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of the jfj
+ * @a: The fid dkkd jjfk
+ * @b: The djd skksuys iis
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+/* th e dudi sjjk sjs */
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
