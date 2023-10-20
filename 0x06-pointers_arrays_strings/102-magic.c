117828868975b338c618968573836cc0c295b5f0
BlassedHillary <nnamanihillaryuchenna@gmail.com>
2023-10-19 20:15:03 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+/*
+ * write ypu ksksnz zkk
+ * Remember:
+ * - utufhf dxiudis sisi
+ * - fihfifw fcwoic
+ * - edwifdhof fcifiic civcwm vwhwf
+ * - wf-0vjc wrco
+ * - fhw vwcovwvw vcwovwjvw vwowvwf fdonvfkf
+ * main: dusuis sih,aoqo.
+ * void: djdjd sisjs sosjs skqw
+ */
+
+	*(p = 5) = 98;
+	/* .... so the sjjs 98\n */
+	printf("a[2] - %d\n", a[2]);
+	return (0);
+}
