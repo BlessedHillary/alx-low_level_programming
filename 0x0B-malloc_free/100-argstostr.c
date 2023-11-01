6b0fc59d4168534d5188fe695bc088c4cca760a1
BlassedHillary <nnamanihillaryuchenna@gmail.com>
2023-10-31 17:05:15 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,10 +27,10 @@ char *argstostr(int ac, char **av)
 			j++;
 		}
 		j = 0;
-		i ++;
+		i++;
 	}
 
-	s = malloc((sizeof(char) * ch) + ac +1);
+	s = malloc((sizeof(char) * ch) + ac + 1);
 
 	i = 0;
 	while (av[i])
